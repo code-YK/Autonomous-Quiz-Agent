5ad0142308ba8794ad5b317f0dca78ba7b4eb55d
code-YK <kpyadav2005@gmail.com>
2025-12-21 11:30:59 +0530

    Prompt changed to obtain summary

--- agents/prompts/extract_prompt.py
+++ agents/prompts/extract_prompt.py
@@ -4,10 +4,11 @@ You are an educational content analyst.
 From the given text:
 - Extract ONLY key concepts
 - Focus on definitions, principles, and important terms
-- Do NOT summarize
+- summarize : 1–2 sentence intuitive explanation (student-friendly)
 - Return output as JSON list with:
   - name
   - description
+  - summary
 
 Text:
 {chunk}
